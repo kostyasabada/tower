2db7c46f9fa5498460722cc572c4665992c920a0
Kostya Sabada <kostyasabada@gmail.com>
2020-12-10 16:49:51 +0200

    delete api

--- src/api/getUsers.js
+++
@@ -1,7 +0,0 @@
-export const getUsersFromServer = async () => {
-  const users = await fetch(`${process.env.PUBLIC_URL}/api/users.json`)
-    .then((res) => res.json())
-    .then((data) => data.users);
-
-  return users;
-};
